260d39a029e14053786039ed60ce6bfad405d27e
Doyup Kim <d25kim@uwaterloo.ca>
2024-06-09 02:51:09 -0400

    made component ListGroup

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,16 @@
+function ListGroup() {
+	const items = ["New York", "San Fransisco", "Tokyo", "London", "Paris"];
+
+	return (
+		<>
+			<h1>Heading</h1>
+			<ul className="list-group">
+				{items.map((item) => (
+					<li key={item}>{item}</li>
+				))}
+			</ul>
+		</>
+	);
+}
+
+export default ListGroup;
