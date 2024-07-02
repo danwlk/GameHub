75677b6aa00971488a4b582bf2f82223f832df04
Doyup Kim <d25kim@uwaterloo.ca>
2024-07-01 23:55:41 -0400

    added alert component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
+import ListGroup from "./components/ListGroup";
 
 function App() {
+	const [showAlert, setShowAlert] = useState(false);
+
 	return (
 		<div>
-			<Button onClick={() => console.log("clicked")} color="danger">
+			{showAlert && (
+				<Alert onClose={() => setShowAlert(false)} warning="WARNING">
+					this is an alert
+				</Alert>
+			)}
+			<Button onClick={() => setShowAlert(true)} color="danger">
 				my button
 			</Button>
+
+			<ListGroup
+				items={["one", "two", "three", "four"]}
+				heading="numbers"
+				onSelectItem={(item) => console.log(item)}
+			/>
 		</div>
 	);
 }
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,10 +2,36 @@ import { ReactNode } from "react";
 
 interface Props {
 	children: ReactNode;
+	color?:
+		| "primary"
+		| "secondary"
+		| "danger"
+		| "success"
+		| "warning"
+		| "info"
+		| "light"
+		| "dark";
+	onClose: () => void;
+	warning?: string;
 }
 
-const Alert = ({ children }: Props) => {
-	return <div className="alert alert-primary">{children}</div>;
+const Alert = ({ children, color = "primary", onClose, warning }: Props) => {
+	return (
+		<div
+			className={"alert alert-" + color + " alert-dismissible fade show"}
+			role="alert"
+		>
+			<strong>{warning}</strong>
+			{" " + children}
+			<button
+				onClick={onClose}
+				type="button"
+				className="btn-close"
+				data-dismiss="alert"
+				aria-label="Close"
+			></button>
+		</div>
+	);
 };
 
 export default Alert;
