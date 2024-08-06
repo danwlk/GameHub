1119b9c9f88c23c947bb7544fe5983a7a4c2d225
Doyup Kim <d25kim@uwaterloo.ca>
2024-08-06 08:41:56 -0400

    state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import Button from "./components/Button/Button";
 import Like from "./components/Like/Like";
 
 function App() {
+	const [customer, setCustomer] = useState({
+		name: "John",
+		address: {
+			city: "San Francisco",
+			zipCode: 94111,
+		},
+	});
+
+	const handleClick = () => {
+		setCustomer({
+			...customer,
+			address: { ...customer.address, zipCode: 94112 },
+		});
+	};
+
 	return (
 		<div>
-			<Button color="primary" onClick={() => {}}>
+			{customer.name}
+			<Button color="primary" onClick={handleClick}>
 				hello world
 			</Button>
 			<Like onClick={() => console.log("clicked")} />
